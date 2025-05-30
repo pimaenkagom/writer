cc64966500b17486e736910810a51ac8fa790598
Miena Basta Badres <dev@mbadres.com>
2025-05-31 00:46:25 +0200

    Add Footer component with licensing information.

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Coptic from '../languages/Coptic.svelte';
+</script>
+
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>
+			<Coptic>Ⲡⲓⲙⲁⲛ̀ⲕⲁϫⲱⲙ</Coptic>. The source code is licensed under
+			<a href="https://opensource.org/license/agpl-v3">
+				GNU Affero General Public License version 3
+			</a>.
+		</p>
+	</div>
+</footer>
