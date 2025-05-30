7e5be1a9c63b07d73283f90fc3bdd7b732b8cf6f
Miena Basta Badres <dev@mbadres.com>
2025-05-31 00:47:15 +0200

    WIP Add login feature.

---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { auth } from '$lib/firebase';
+	import type { User } from 'firebase/auth';
+	import {
+		createUserWithEmailAndPassword,
+		signInWithEmailAndPassword,
+		signOut
+	} from 'firebase/auth';
+
+	let user: User | null = $state(null);
+	let loading = $state(true);
+	let email = $state('');
+	let password = $state('');
+	let isRegistering = $state(false);
+	let errorMessage = $state('');
+
+	$effect(() => {
+		const unsubscribe = auth.onAuthStateChanged((userData) => {
+			user = userData;
+			loading = false;
+		});
+
+		return () => unsubscribe();
+	});
+
+	async function handleSubmit() {
+		try {
+			errorMessage = '';
+			if (isRegistering) {
+				await createUserWithEmailAndPassword(auth, email, password);
+			} else {
+				await signInWithEmailAndPassword(auth, email, password);
+			}
+			email = '';
+			password = '';
+		} catch (error: any) {
+			errorMessage = error.message;
+		}
+	}
+</script>
+
+{#if loading}
+	<p>Loading...</p>
+{:else if user}
+	<div>
+		<p class="subtitle">Willkommen, {user.email}!</p>
+		<button class="button" onclick={() => signOut(auth)}>Ausloggen</button>
+	</div>
+{:else}
+	<form
+		onsubmit={(e) => {
+			e.preventDefault();
+			handleSubmit();
+		}}
+	>
+		<h2 class="title">{isRegistering ? 'Registrieren' : 'Anmelden'}</h2>
+
+		{#if errorMessage}
+			<p class="subtitle">{errorMessage}</p>
+		{/if}
+
+		<button class="button" type="button" onclick={() => (isRegistering = !isRegistering)}>
+			{isRegistering ? 'Bereits einen Account? Anmelden' : 'Neuer Benutzer? Registrieren'}
+		</button>
+	</form>
+
+	<div class="field">
+		<p class="control has-icons-left">
+			<span class="icon is-small is-left">
+				<i class="fas fa-envelope"></i>
+			</span>
+			<input class="input" type="email" placeholder="Email" required bind:value={email} />
+		</p>
+	</div>
+	<div class="field">
+		<p class="control has-icons-left">
+			<span class="icon is-small is-left">
+				<i class="fas fa-lock"></i>
+			</span>
+			<input class="input" type="password" placeholder="Password" required bind:value={password} />
+		</p>
+	</div>
+	<div class="field">
+		<p class="control">
+			<button class="button is-success" onclick={handleSubmit}>
+				{isRegistering ? 'Sign up' : 'Log in'}
+			</button>
+		</p>
+	</div>
+{/if}
