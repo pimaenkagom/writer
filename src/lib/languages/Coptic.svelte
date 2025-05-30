9568ac327c3d31145fb95ed2c9174fd4eb00bc77
Miena Basta Badres <dev@mbadres.com>
2025-05-31 00:44:21 +0200

    Add a Svelte component for Coptic text.

---
+++ src/lib/languages/Coptic.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<span class="noto-sans-coptic-regular">
+	{@render children?.()}
+</span>
+
+<style>
+	@font-face {
+		font-family: 'Noto Sans Coptic';
+		font-style: normal;
+		font-weight: 400;
+		src: local('Noto Sans Coptic'), url(/fonts/noto-sans-coptic-regular.ttf);
+	}
+
+	.noto-sans-coptic-regular {
+		font-family: 'Noto Sans Coptic';
+	}
+</style>
