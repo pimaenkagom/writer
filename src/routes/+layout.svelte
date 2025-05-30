fbee21855dfcc4062b97b537d5269087e1d6fefe
Miena Basta Badres <dev@mbadres.com>
2025-05-31 00:43:07 +0200

    Refactor layout structure to include Header and Footer components.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="is-flex is-flex-direction-column is-fullheight-custom">
+	<Header />
+	<div class=" is-flex-grow-1">
+		{@render children()}
+	</div>
+	<Footer />
+</div>
+
+<style>
+	.is-fullheight-custom {
+		min-height: 100dvh;
+	}
+</style>
