e82463e2e3d90f251a22bd308bd45d8c1f53b4dd
Miena Basta Badres <dev@mbadres.com>
2025-12-09 00:19:02 +0100

    Add new page layout for library navigation and remove old page structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { settings } from '$lib/states/helpers/settings.svelte';
+
+	const currentLibrary = $derived(settings.value.library.current);
+	const currentCollection = $derived(settings.value.collection.current);
+	const currentBook = $derived(settings.value.book.current);
+	const currentPart = $derived(settings.value.part.current);
+	const currentChapter = $derived(settings.value.chapter.current);
+	const currentSection = $derived(settings.value.section.current);
+	const currentParagraph = $derived(settings.value.paragraph.current);
+	const currentClause = $derived(settings.value.clause.current);
+</script>
+
+<section class="section">
+	<div class="container">{currentLibrary}</div>
+	<div class="container">{currentCollection}</div>
+	<div class="container">{currentBook}</div>
+	<div class="container">{currentPart}</div>
+	<div class="container">{currentChapter}</div>
+	<div class="container">{currentSection}</div>
+	<div class="container">{currentParagraph}</div>
+	<div class="container">{currentClause}</div>
+</section>
+
+<section class="section">
+	<div class="container">
+		This is clause {currentClause} of paragraph {currentParagraph} of section
+		{currentSection} of chapter {currentChapter} of part {currentPart} of book {currentBook} of collection
+		{currentCollection} of library {currentLibrary}.
+	</div>
+</section>
