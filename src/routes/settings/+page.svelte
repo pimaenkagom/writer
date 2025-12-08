09e977fd28883df82a4be60ce1a6ff778d986bd4
Miena Basta Badres <dev@mbadres.com>
2025-12-09 00:47:09 +0100

    Fix settings display logic to ensure only shown settings are rendered

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -7,36 +7,38 @@
 	<div class="container">
 		<h1 class="title">Settings</h1>
 		{#each Object.values(settings.value) as setting}
-			<div class="field">
-				<label class="label" for={setting.key}>{capitalize(setting.key)}</label>
-				<div class="control is-expanded">
-					{#if setting.availables.length === 0}
-						<input
-							class="input"
-							type="text"
-							id={setting.key}
-							bind:value={setting.current}
-							on:change={() => {
-								setSetting(setting.key, setting.current);
-							}}
-						/>
-					{:else}
-						<div class="select is-fullwidth">
-							<select
+			{#if setting.shown}
+				<div class="field">
+					<label class="label" for={setting.key}>{capitalize(setting.key)}</label>
+					<div class="control is-expanded">
+						{#if setting.availables.length === 0}
+							<input
+								class="input"
+								type="text"
 								id={setting.key}
 								bind:value={setting.current}
 								on:change={() => {
 									setSetting(setting.key, setting.current);
 								}}
-							>
-								{#each setting.availables as option}
-									<option value={option}>{option}</option>
-								{/each}
-							</select>
-						</div>
-					{/if}
+							/>
+						{:else}
+							<div class="select is-fullwidth">
+								<select
+									id={setting.key}
+									bind:value={setting.current}
+									on:change={() => {
+										setSetting(setting.key, setting.current);
+									}}
+								>
+									{#each setting.availables as option}
+										<option value={option}>{option}</option>
+									{/each}
+								</select>
+							</div>
+						{/if}
+					</div>
 				</div>
-			</div>
+			{/if}
 		{/each}
 	</div>
 </section>
