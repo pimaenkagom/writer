c7b08423a735be6f711d25321f64d3e57358654b
Miena Basta Badres <dev@mbadres.com>
2025-12-09 00:18:32 +0100

    Enhance settings UI.

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -9,20 +9,32 @@
 		{#each Object.values(settings.value) as setting}
 			<div class="field">
 				<label class="label" for={setting.key}>{capitalize(setting.key)}</label>
-				<div class="control">
-					<div class="select">
-						<select
+				<div class="control is-expanded">
+					{#if setting.availables.length === 0}
+						<input
+							class="input"
+							type="text"
 							id={setting.key}
 							bind:value={setting.current}
 							on:change={() => {
 								setSetting(setting.key, setting.current);
 							}}
-						>
-							{#each setting.availables as option}
-								<option value={option}>{option}</option>
-							{/each}
-						</select>
-					</div>
+						/>
+					{:else}
+						<div class="select is-fullwidth">
+							<select
+								id={setting.key}
+								bind:value={setting.current}
+								on:change={() => {
+									setSetting(setting.key, setting.current);
+								}}
+							>
+								{#each setting.availables as option}
+									<option value={option}>{option}</option>
+								{/each}
+							</select>
+						</div>
+					{/if}
 				</div>
 			</div>
 		{/each}
