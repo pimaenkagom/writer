9607921cc98ca142c68d9025afa7af2085e68d56
Miena Basta Badres <dev@mbadres.com>
2025-12-21 22:14:36 +0100

    Implement language selection management with derived state and settings integration

---
+++ src/lib/components/helpers/Languages.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { delimiter, getSelectedLanguages } from '$lib/states/contents/languages.svelte';
+	import { setSetting } from '$lib/states/helpers/settings.svelte';
+
+	$effect(() => {
+		setSetting('languages', getSelectedLanguages().join(delimiter));
+	});
+</script>
