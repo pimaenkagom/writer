3e61bf208d56cc228c21d79c551bfa1b6de5578d
Miena Basta Badres <dev@mbadres.com>
2025-12-28 00:30:36 +0100

    Create empty node editor

--- src/lib/components/nodes/helper/EditWrapper.svelte
+++ src/lib/components/nodes/helper/EditWrapper.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
 	import Selector from '$lib/components/nodes/helper/Selector.svelte';
+	import Modal from '$lib/layout/Modal.svelte';
 	import type { Basenode } from '$lib/models/helpers/basenode.model';
+	import Editor from './Editor.svelte';
 
 	let { model, language }: { model: Basenode; language?: string | undefined } = $props();
+
+	let isEditing = $state(false);
+
+	function openEditor() {
+		isEditing = true;
+	}
 </script>
 
 <div class="columns">
-	<div class="column is-narrow">Edit</div>
+	<div class="column is-narrow">
+		<button class="button" aria-label="Open Editor" onclick={openEditor}>
+			<span class="icon is-small">
+				<i class="fa-solid fa-pen"></i>
+			</span>
+		</button>
+		<Modal bind:isActive={isEditing}>
+			<Editor {model} />
+		</Modal>
+	</div>
 	<div class="column">
 		<Selector {model} {language} />
 	</div>
---
+++ src/lib/components/nodes/helper/Editor.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Basenode } from '$lib/models/helpers/basenode.model';
+
+	let { model }: { model: Basenode } = $props();
+</script>
+
+Node Editor
+
+{model.id}
