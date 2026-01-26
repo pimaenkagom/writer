3c94370a1e8ab6bc3e1516e75882135ca63883c5
Miena Basta Badres <dev@mbadres.com>
2026-01-26 11:39:00 +0100

    Refactor AddNewNode component to use MultilingualTextBrowser and update modal handling

--- src/lib/components/nodes/helper/AddNewNode.svelte
+++ src/lib/components/nodes/helper/AddNewNode.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
-	import MultilingualTextEditor from '$lib/components/contents/mulltilinual-text/MultilingualTextEditor.svelte';
+	import MultilingualTextBrowser from '$lib/components/contents/mulltilinual-text/MultilingualTextBrowser.svelte';
 	import Modal from '$lib/components/layout/Modal.svelte';
+	import { getCollectionForType } from '$lib/states/nodes/nodes.svelte';
 
-	let openModal = $state(false);
+	const { type }: { type: string } = $props();
 
-	function addNew() {
-		openModal = true;
+	let isActive = $state(false);
+
+	function openBrowser() {
+		isActive = true;
+	}
+
+	function select(id: string) {
+		const collections = getCollectionForType(type);
+		collections.create({
+			users: [],
+			type: type,
+			value: id,
+			shown: true,
+			tags: [],
+			children: [],
+			childrenTypes: []
+		});
+
+		isActive = false;
 	}
 </script>
 
-<button class="button" onclick={addNew}>
+<button class="button" onclick={openBrowser}>
 	<span class="icon">
 		<i class="fa-solid fa-plus"></i>
 	</span>
 	<span>New</span>
 </button>
 
-<Modal title="Editor" bind:isActive={openModal}>
-	<MultilingualTextEditor bind:isNotSaved={openModal} />
+<Modal title="Editor" bind:isActive isWide={true}>
+	<MultilingualTextBrowser isSelected={select} />
 </Modal>
