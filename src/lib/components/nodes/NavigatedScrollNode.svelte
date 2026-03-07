e4179fb24554257d44b64f6b9b4d6004df6ab35d
Miena Basta Badres <dev@mbadres.com>
2026-03-07 11:03:48 +0100

    Refactor the navigated scroll node.

--- src/lib/components/nodes/NavigatedScrollNode.svelte
+++ src/lib/components/nodes/NavigatedScrollNode.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import ChildNodes from '$lib/components/nodes/helper/ChildNodes.svelte';
 	import ChildrenNavigation from '$lib/components/nodes/helper/ChildrenNavigation.svelte';
-	import NodeHead from '$lib/components/nodes/helper/NodeHead.svelte';
+	import ScrollNode from '$lib/components/nodes/ScrollNode.svelte';
 	import type { Basenode } from '$lib/models/helpers/basenode.model';
 
 	const { model }: { model: Basenode } = $props();
@@ -12,11 +11,6 @@
 		<ChildrenNavigation {model} />
 	</div>
 	<div class="column">
-		<NodeHead {model} />
-		<section class="section">
-			<div class="container">
-				<ChildNodes {model} />
-			</div>
-		</section>
+		<ScrollNode {model} />
 	</div>
 </div>
