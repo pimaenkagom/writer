01e70caa6fc5fe6a56cb234b4920e5d0ea78cc37
Miena Basta Badres <dev@mbadres.com>
2026-03-07 20:11:19 +0100

    Remove the detector node and refactor.

--- src/lib/components/header/Executer.svelte
+++ src/lib/components/header/Executer.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import { Type } from '$lib/models/helpers/type.model';
-	import { libraries } from '$lib/states/nodes/nodes.svelte';
-
 	function execute() {
 		console.log('Execute button clicked');
-
+		/*
 		libraries.create({
 			users: ['5C5AB17F-A194-4123-A4A0-D26CE5B0AA08'],
 			type: Type.Library,
@@ -14,6 +11,7 @@
 			children: [],
 			childrenTypes: []
 		});
+	*/
 	}
 </script>
 
