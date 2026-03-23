bfff83f6829d90ba3fe61c4bd7dc1eed8af21f19
Miena Basta Badres <dev@mbadres.com>
2026-03-23 23:51:58 +0100

    Improve loading speed.

--- src/lib/components/nodes/helper/EditNodeButton.svelte
+++ src/lib/components/nodes/helper/EditNodeButton.svelte
@@ -3,38 +3,45 @@
 	import Modal from '$lib/components/layout/Modal.svelte';
 	import Editor from '$lib/components/nodes/helper/Editor.svelte';
 	import type { Basenode } from '$lib/models/basenode.model';
+	import { NodeType } from '$lib/models/node-type.model';
 	import { Role } from '$lib/models/roles.model';
 	import { getCollectionForNodeType } from '$lib/states/nodes.svelte';
 
 	const { model }: { model: Basenode } = $props();
 
-	// svelte-ignore state_referenced_locally
-	const snapshot = $state($state.snapshot(model));
+	let snapshot = $state<Basenode | null>(null);
 
 	let isEditing = $state(false);
 
 	function openEditor() {
+		snapshot = $state.snapshot(model);
 		isEditing = true;
 	}
 
 	function save() {
-		getCollectionForNodeType(model.type).update(snapshot.id, snapshot);
+		getCollectionForNodeType(model.type).update(snapshot!.id, snapshot!);
 	}
 </script>
 
 <AuthGuard roles={[Role.Writer]}>
-	<button class="button" aria-label="Open Editor" onclick={openEditor}>
+	<button
+		class:button={model.type !== NodeType.Clause}
+		aria-label="Open Editor"
+		onclick={openEditor}
+	>
 		<span class="icon is-small">
 			<i class="fa-solid fa-pen"></i>
 		</span>
 	</button>
-	<Modal
-		bind:isActive={isEditing}
-		title="Edit Node"
-		isWide={true}
-		saveFunc={save}
-		hasSaveFunc={true}
-	>
-		<Editor model={snapshot} />
-	</Modal>
+	{#if snapshot !== null}
+		<Modal
+			bind:isActive={isEditing}
+			title="Edit Node"
+			isWide={true}
+			saveFunc={save}
+			hasSaveFunc={true}
+		>
+			<Editor model={snapshot} />
+		</Modal>
+	{/if}
 </AuthGuard>
